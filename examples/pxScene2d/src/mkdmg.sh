54c45824b29d280a9ea200c7bd02f48d8e085508
cfry002 <constance_fry@cable.comcast.com>
2017-06-28 07:05:05 -0700

    debugging

--- examples/pxScene2d/src/mkdmg.sh
+++ examples/pxScene2d/src/mkdmg.sh
@@ -139,7 +139,7 @@ createDMG() {
   ICON_SIZE=128	#DMG icon size
   TEXT_SIZE=16	#DMG text size
 
-  VOLUME_NAME="pxscene"	#mounted DMG name
+  VOLUME_NAME=pxscene	#mounted DMG name
   MOUNT_DIR="/Volumes/${VOLUME_NAME}"
   VOLUME_ICON_FILE=${DMG_RES_DIR}/pxscenevolico.icns	#DMG volume icon
   BACKGROUND_FILE=${DMG_RES_DIR}/background.png		#DMG background image
@@ -199,86 +199,15 @@ createDMG() {
     SetFile -c icnC "${MOUNT_DIR}/.VolumeIcon.icns"
     printf "done.\n"
   fi
-  sleep 4
+
   # run applescript
   APPLESCRIPT=$(mktemp -t createdmg)
   printf "  Creating Applescript in ${APPLESCRIPT} for customizing DMG..."
   printf 'on run (volumeName)
 	tell application "Finder"
-		tell disk (volumeName as string)
-			open
-			
-			set theXOrigin to WINX
-			set theYOrigin to WINY
-			set theWidth to WINW
-			set theHeight to WINH
-			
-			set theBottomRightX to (theXOrigin + theWidth)
-			set theBottomRightY to (theYOrigin + theHeight)
-			set dsStore to \"\\\"\" & "/Volumes/" & volumeName & "/" & ".DS_STORE\\\""
-			
-			tell container window
-				set current view to icon view
-				set toolbar visible to false
-				set statusbar visible to false
-				set the bounds to {theXOrigin, theYOrigin, theBottomRightX, theBottomRightY}
-				set statusbar visible to false
-				REPOSITION_HIDDEN_FILES_CLAUSE
-			end tell
-			
-			set opts to the icon view options of container window
-			tell opts
-				set icon size to ICON_SIZE
-				set text size to TEXT_SIZE
-				set arrangement to not arranged
-			end tell
-			BACKGROUND_CLAUSE
-			
-			-- Positioning
-			POSITION_CLAUSE
-			
-			-- Hiding
-			HIDING_CLAUSE
-			
-			-- Application Link Clause
-			APPLICATION_CLAUSE
-            close
-            open
-			
-			update without registering applications
-			-- Force saving of the size
-			delay 1
-			
-			tell container window
-				set statusbar visible to false
-				set the bounds to {theXOrigin, theYOrigin, theBottomRightX - 10, theBottomRightY - 10}
-			end tell
-			
-			update without registering applications
-		end tell
-		
-		delay 1
-		
-		tell disk (volumeName as string)
-			tell container window
-				set statusbar visible to false
-				set the bounds to {theXOrigin, theYOrigin, theBottomRightX, theBottomRightY}
-			end tell
-			
-			update without registering applications
-		end tell
-		
-		--give the finder some time to write the .DS_Store file
-		delay 3
-		
-		set waitTime to 0
-		set ejectMe to false
-		repeat while ejectMe is false
-			delay 1
-			set waitTime to waitTime + 1
-			
-			if (do shell script "[ -f " & dsStore & " ]; echo ${?}") = "0" then set ejectMe to true
-		end repeat
+    get the name of every disk
+      copy result to stdout
+
 	end tell
   end run' | sed -e "s/WINX/${WINX}/g" -e "s/WINY/${WINY}/g" -e "s/WINW/${WINW}/g" -e "s/WINH/${WINH}/g" -e "s/BACKGROUND_CLAUSE/${BACKGROUND_CLAUSE}/g" -e "s/REPOSITION_HIDDEN_FILES_CLAUSE/${REPOSITION_HIDDEN_FILES_CLAUSE}/g" -e "s/ICON_SIZE/${ICON_SIZE}/g" -e "s/TEXT_SIZE/${TEXT_SIZE}/g" | perl -pe  "s/POSITION_CLAUSE/${POSITION_CLAUSE}/g" | perl -pe "s/APPLICATION_CLAUSE/${APPLICATION_CLAUSE}/g" | perl -pe "s/HIDING_CLAUSE/${HIDING_CLAUSE}/" >"${APPLESCRIPT}"
   printf "done.\n"
