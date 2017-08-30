c501d58994775d7b69873887b820b53ad007ff4f
conniefry <constance_fry@cable.comcast.com>
2017-08-29 18:40:00 -0700

    auto merge

---
+++ ci/auto_merge_coverity.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Automatically merge master to coverity_scan branch
+checkError()
+{
+  if [ "$1" -ne 0 ]
+  then
+    echo "auto_merge_coverity failed with errors !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "failure reason: $2"
+    echo "Cause: $3"
+    echo "Reproduction/How to fix: $4"
+    exit 1
+  fi
+}
+
+
+echo Automatically merge master to coverity_scan branch
+
+git checkout coverity_scan && git merge master
+
+git push --repo="https://$REPO_USER_NAME:$GH_TOKEN@github.com/$REPO_USER_NAME/$REPO_NAME.git"
+checkError $? "unable to commit data to repo" "" "check the credentials"
+
