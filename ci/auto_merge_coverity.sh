38c5475a4e488234ae1dfe5674fce9cdce2c3b2c
conniefry <conniefry@users.noreply.github.com>
2017-08-29 20:03:09 -0700

    work around travis clone

--- ci/auto_merge_coverity.sh
+++ ci/auto_merge_coverity.sh
@@ -12,13 +12,33 @@ checkError()
   fi
 }
 
+create_all_branches()
+{
+    # Keep track of where Travis put us.
+    # We are on a detached head, and we need to be able to go back to it.
+    local build_head=$(git rev-parse HEAD)
+
+    # Fetch all the remote branches. Travis clones with `--depth`, which
+    # implies `--single-branch`, so we need to overwrite remote.origin.fetch to
+    # do that.
+    git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+    git fetch
+    # optionally, we can also fetch the tags
+    # git fetch --tags
+
+    # reate the tracking branchescreate the tracking branches
+    #for branch in $(git branch -r|grep -v HEAD) ; do
+    #    git checkout -qf ${branch#origin/}
+    #done
+
+    # finally, go back to where we were at the beginning
+    git checkout ${build_head}
+}
 
 echo Automatically merge master to coverity_scan branch
 
-git fetch origin
-git checkout jr_master
+#git checkout jr_master
 git checkout coverity_scan && git merge jr_master
 
 git push --repo="https://$REPO_USER_NAME:$GH_TOKEN@github.com/$REPO_USER_NAME/$REPO_NAME.git"
 checkError $? "unable to commit data to repo" "" "check the credentials"
-
