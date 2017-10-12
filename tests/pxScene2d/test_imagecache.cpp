abd06af5d30af4ff47b29eea2b7808b9d8922c38
conniefry <constance_fry@cable.comcast.com>
2017-10-12 06:20:08 -0700

    remove printfs

--- tests/pxScene2d/test_imagecache.cpp
+++ tests/pxScene2d/test_imagecache.cpp
@@ -556,7 +556,6 @@ class rtHttpCacheTest : public testing::Test, public commonTestFns
 
    void dataPresentAfterHeadersRevalidationTest()
    {
-     printf("dataPresentAfterHeadersRevalidationTest\n");
      const char* cacheHeader = "HTTP/1.1 200 OK\nDate: Sun, 09 Oct 2015 21:22:50 GMT\nServer: Apache/2.4.7 (Ubuntu)\nLast-Modified: Sat, 08 Oct 2015 02:46:40 GMT\nETag: \"fb4-53e51895552f0\"\nAccept-Ranges: bytes\nContent-Length: 4020\nExpires: Mon, 10 Oct 2017 21:22:50 GMT\nContent-Type: image/jpeg\nCache-Control: public no-cache=Expires\n\0";
      const char* cacheData = "abcde";
      addDataToCache("http://localhost/test.jpeg",cacheHeader,cacheData,strlen(cacheData));
@@ -578,7 +577,6 @@ class rtHttpCacheTest : public testing::Test, public commonTestFns
 
    void dataPresentAfterFullRevalidationTest()
    {
-     printf("dataPresentAfterFullRevalidationTest\n");
      const char* cacheHeader = "HTTP/1.1 200 OK\nDate: Sun, 09 Oct 2015 21:22:50 GMT\nServer: Apache/2.4.7 (Ubuntu)\nLast-Modified: Sat, 08 Oct 2015 02:46:40 GMT\nETag: \"fb4-53e51895552f0\"\nAccept-Ranges: bytes\nContent-Length: 4020\nCache-Control: no-cache\nExpires: Mon, 10 Oct 2017 21:22:50 GMT\nContent-Type: image/jpeg\n\0";
      const char* cacheData = "abcde";
      addDataToCache("http://localhost/test1.jpeg",cacheHeader,cacheData,strlen(cacheData));
@@ -600,7 +598,6 @@ class rtHttpCacheTest : public testing::Test, public commonTestFns
 
    void dataUpdatedAfterFullRevalidationTest()
    {
-     printf("dataUpdatedAfterFullRevalidationTest\n");
      const char* cacheHeader = "HTTP/1.1 200 OK\nDate: Sun, 09 Oct 2015 21:22:50 GMT\nServer: Apache/2.4.7 (Ubuntu)\nLast-Modified: Sat, 08 Oct 2015 02:46:40 GMT\nETag: \"fb4-53e51895552f0\"\nAccept-Ranges: bytes\nContent-Length: 4020\nCache-Control: no-cache\nExpires: Mon, 10 Oct 2017 21:22:50 GMT\nContent-Type: image/jpeg\n\0";
      const char* cacheData = "abcde";
      addDataToCache("http://localhost/testRevalidationUpdate",cacheHeader,cacheData,strlen(cacheData));
@@ -623,7 +620,6 @@ class rtHttpCacheTest : public testing::Test, public commonTestFns
 
    void dataNotUpdatedAfterFullRevalidationTest()
    {
-     printf("dataNotUpdatedAfterFullRevalidationTest\n");
      const char* cacheHeader = "HTTP/1.1 200 OK\nDate: Sun, 09 Oct 2015 21:22:50 GMT\nServer: Apache/2.4.7 (Ubuntu)\nLast-Modified: Sat, 08 Oct 2015 02:46:40 GMT\nETag: \"fb4-53e51895552f0\"\nAccept-Ranges: bytes\nContent-Length: 4020\nCache-Control: no-cache\nExpires: Mon, 10 Oct 2017 21:22:50 GMT\nContent-Type: image/jpeg\n\0";
      const char* cacheData = "abcde";
      addDataToCache("http://localhost/testRevalidationUpdateFailed",cacheHeader,cacheData,strlen(cacheData));
@@ -635,7 +631,6 @@ class rtHttpCacheTest : public testing::Test, public commonTestFns
 
     void dataUpdatedAfterEtagTest()
     {
-       printf("dataUpdatedAfterEtagTest\n");
       const char* cacheHeader = "HTTP/1.1 200 OK\nDate: Sun, 09 Oct 2016 21:22:50 GMT\nServer: Apache/2.4.7 (Ubuntu)\nLast-Modified: Sat, 08 Oct 2017 02:46:40 GMT\nETag: \"fb4-53e51895552f0\"\nAccept-Ranges: bytes\nContent-Length: 4020\nCache-Control: public\nExpires: Mon, 10 Oct 2017 21:22:50 GMT\nContent-Type: image/jpeg\n\0";
      const char* cacheData = "abcde";
      addDataToCache("http://localhost/testEtag",cacheHeader,cacheData,strlen(cacheData));
